880b9573cc26a6483ed50cc437e667d9f67b0e4e
Greg Bruce <bruce@thinkflipp.com>
2019-01-11 09:41:22 -0700

    The atom finder code :D

---
+++ spec/atom-finder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomFinderView from '../lib/atom-finder-view';
+
+describe('AtomFinderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
